b0d7d120c9d6eced1495e23c89029a5d1961b08b
g2xpf <nameko.jack@gmail.com>
2020-05-22 18:09:14 +0900

    Bitmap の表示

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -27,10 +28,15 @@ public class MainActivity extends AppCompatActivity {
         photoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                // TODO: You should setup appropriate parameters for the intent
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
                 PackageManager manager = getPackageManager();
+
+                if(!manager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+                    Toast.makeText(MainActivity.this, "No cameras detected", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
                 List activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
                 if (!activities.isEmpty()) {
                     startActivityForResult(intent, REQ_PHOTO);
@@ -54,7 +60,10 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(reqCode, resCode, data);
         if (reqCode == REQ_PHOTO) {
             if (resCode == RESULT_OK) {
-                // TODO: You should implement the code that retrieve a bitmap image
+                Bundle extras = data.getExtras();
+                if(extras != null) {
+                    photoImage = (Bitmap) extras.get("data");
+                }
             }
         }
     }
