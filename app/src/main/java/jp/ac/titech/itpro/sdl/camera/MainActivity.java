174bc1a5e12ceb109f4d897a1e03b6d593a41b9c
g2xpf <nameko.jack@gmail.com>
2020-05-22 19:43:19 +0900

    高精細な画像の表示

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
@@ -3,7 +3,9 @@ package jp.ac.titech.itpro.sdl.camera;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -11,13 +13,17 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int REQ_PHOTO = 1234;
     private Bitmap photoImage = null;
+    private String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +38,27 @@ public class MainActivity extends AppCompatActivity {
 
                 PackageManager manager = getPackageManager();
 
-                if(!manager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+                if (!manager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
                     Toast.makeText(MainActivity.this, "No cameras detected", Toast.LENGTH_LONG).show();
                     return;
                 }
 
+                File photoFile = null;
+                try {
+                    File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                    photoFile = File.createTempFile("tmp", ".jpg", storageDir);
+                    currentPhotoPath = photoFile.getAbsolutePath();
+                } catch (IOException e) {
+                    Toast.makeText(MainActivity.this, "Failed to create a temporary file", Toast.LENGTH_LONG).show();
+                }
+
                 List activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
                 if (!activities.isEmpty()) {
-                    startActivityForResult(intent, REQ_PHOTO);
+                    if (photoFile != null) {
+                        Uri photoURI = FileProvider.getUriForFile(MainActivity.this, "com.example.android.fileprovider", photoFile);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(intent, REQ_PHOTO);
+                    }
                 } else {
                     Toast.makeText(MainActivity.this, R.string.toast_no_activities, Toast.LENGTH_LONG).show();
                 }
@@ -60,9 +79,12 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(reqCode, resCode, data);
         if (reqCode == REQ_PHOTO) {
             if (resCode == RESULT_OK) {
-                Bundle extras = data.getExtras();
-                if(extras != null) {
-                    photoImage = (Bitmap) extras.get("data");
+                File f = new File(currentPhotoPath);
+                Uri contentUri = Uri.fromFile(f);
+                try {
+                    photoImage = MediaStore.Images.Media.getBitmap(getContentResolver(), contentUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         }
